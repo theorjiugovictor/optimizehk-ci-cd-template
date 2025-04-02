a8f6bac8962245f7bbe28776ba47003440e25c96
Orjiugo Victor <orjiugo.victor@gmail.com>
2025-04-03 01:20:39 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// A simple Node.js server  
+const http = require('http');  
+const server = http.createServer((req, res) => {  
+  res.writeHead(200, { 'Content-Type': 'text/plain' });  
+  res.end('Hello, CI/CD Pipeline!');  
+});  
+server.listen(3000, () => console.log('Server running on port 3000'));  
